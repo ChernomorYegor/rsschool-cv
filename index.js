77aab2676e794a43f99889be29789a32710687d5
ChernomorYegor <chernomor.yegor@gmail.com>
2021-09-20 17:47:04 +0300

    feat: add header with menu

---
+++ index.js
@@ -0,0 +1,8 @@
+// Menu
+const menu = document.querySelector('.menu');
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', function() {
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
