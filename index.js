3fa7165229081f6e808cf53bfe866a4612ad1d12
ChernomorYegor <chernomor.yegor@gmail.com>
2021-09-21 01:22:29 +0300

    fix: remove bugs in the burger menu

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Menu
+const body = document.querySelector('body');
 const menu = document.querySelector('.menu');
 const menuToggle = document.querySelector('.menu-toggle');
 
-menuToggle.addEventListener('click', function() {
+function toggleMenu() {
   menu.classList.toggle('active');
   menuToggle.classList.toggle('active');
-});
+  body.classList.toggle('lock');
+}
+
+function closeMenu() {
+  menu.classList.remove('active');
+  menuToggle.classList.remove('active');
+  body.classList.remove('lock');
+}
+
+menuToggle.addEventListener('click', toggleMenu);
+menu.addEventListener('click', closeMenu);
